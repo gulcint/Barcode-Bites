ccb5c635ce4baec1a016d95fb97666c64bbe9d69
gulcint <contact@gulcint.com.tr>
2026-03-01 01:04:04 +0300

    chore(shared): add SQLDelight plugin and runtime foundation

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     kotlin("android") version "2.0.21" apply false
     kotlin("multiplatform") version "2.0.21" apply false
     kotlin("plugin.serialization") version "2.0.21" apply false
+    id("com.android.library") version "8.7.3" apply false
+    id("app.cash.sqldelight") version "2.0.2" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     id("com.android.application") version "8.7.3" apply false
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,14 +1,18 @@
 plugins {
     kotlin("multiplatform")
     kotlin("plugin.serialization")
+    id("com.android.library")
+    id("app.cash.sqldelight")
 }
 
 kotlin {
+    androidTarget()
     jvm()
 
     sourceSets {
         val commonMain by getting {
             dependencies {
+                implementation("app.cash.sqldelight:runtime:2.0.2")
                 implementation("io.ktor:ktor-client-core:2.3.12")
                 implementation("io.ktor:ktor-client-content-negotiation:2.3.12")
                 implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.12")
@@ -24,12 +28,41 @@ kotlin {
             }
         }
 
+        val androidMain by getting {
+            dependencies {
+                implementation("app.cash.sqldelight:android-driver:2.0.2")
+            }
+        }
+
         val jvmMain by getting {
             dependencies {
                 implementation("io.ktor:ktor-client-okhttp:2.3.12")
+            }
+        }
+
+        val jvmTest by getting {
+            dependencies {
+                implementation("app.cash.sqldelight:sqlite-driver:2.0.2")
             }
         }
     }
 }
 
 kotlin.jvmToolchain(17)
+
+android {
+    namespace = "com.barcodebite.shared"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+    }
+}
+
+sqldelight {
+    databases {
+        create("BarcodeBiteDatabase") {
+            packageName.set("com.barcodebite.shared.local")
+        }
+    }
+}
